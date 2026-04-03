6f3a8ce1002eea1aad3f811f72bf628616f0d906
siraphob sirichiraphan <75577213+SiraphobS@users.noreply.github.com>
2026-04-04 01:39:06 +0700

    fix: set image folder as public asset directory

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   // ตั้งค่า Base สำคัญมากสำหรับรันบน GitHub Pages (ชื่อ repository ของคุณ)
   base: '/gunshadelivery/',
+  publicDir: 'image', // กำหนดให้โฟลเดอร์ image เป็นโฟลเดอร์ public
 })
